ffbe3aedad48cd9ce53c994da1602a5c558e9752
Dan Paulat <dpaulat@gmail.com>
2025-05-28 19:59:33 -0500

    setup-multi.sh is not supported on Linux

--- tools/setup-multi.sh
+++ tools/setup-multi.sh
@@ -7,5 +7,9 @@ export generator="Ninja Multi-Config"
 export qt_base=/opt/Qt
 export qt_arch=gcc_64
 
+# FIXME: aws-sdk-cpp fails to configure using Ninja Multi-Config
+echo "Ninja Multi-Config is not supported in Linux"
+read -p "Press Enter to continue..."
+
 # Perform common setup
-${script_dir}/lib/setup-common.sh
+# ${script_dir}/lib/setup-common.sh
