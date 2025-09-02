5029f6dc6fcf041009fd5f42a0555b4903346fa7
Dan Paulat <dpaulat@gmail.com>
2025-09-01 22:25:16 -0500

    Provide NWS types file for NWS API responses

--- wxdata/wxdata.cmake
+++ wxdata/wxdata.cmake
@@ -83,9 +83,11 @@ set(SRC_PROVIDER source/scwx/provider/aws_level2_data_provider.cpp
                  source/scwx/provider/nexrad_data_provider_factory.cpp
                  source/scwx/provider/warnings_provider.cpp)
 set(HDR_TYPES include/scwx/types/iem_types.hpp
-              include/scwx/types/ntp_types.hpp)
+              include/scwx/types/ntp_types.hpp
+              include/scwx/types/nws_types.hpp)
 set(SRC_TYPES source/scwx/types/iem_types.cpp
-              source/scwx/types/ntp_types.cpp)
+              source/scwx/types/ntp_types.cpp
+              source/scwx/types/nws_types.cpp)
 set(HDR_UTIL include/scwx/util/digest.hpp
              include/scwx/util/enum.hpp
              include/scwx/util/environment.hpp
