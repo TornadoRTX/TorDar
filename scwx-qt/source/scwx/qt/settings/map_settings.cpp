ac396586f71bde3eb7cddace87d5a4526991fd54
TornadoRTX <143273950+TornadoRTX@users.noreply.github.com>
2025-09-27 16:48:34 -0400

    Map_settings.cpp: Reverted to older version.

--- scwx-qt/source/scwx/qt/settings/map_settings.cpp
+++ scwx-qt/source/scwx/qt/settings/map_settings.cpp
@@ -86,10 +86,8 @@ public:
                }
                else
                {
-                  // Validate level 3 product
-                  const auto level3Product =
-                     common::GetLevel3ProductByAwipsId(value);
-                  return !level3Product.empty() && level3Product != "?";
+                  // TODO: Validate level 3 product
+                  return true;
                }
             });
 
