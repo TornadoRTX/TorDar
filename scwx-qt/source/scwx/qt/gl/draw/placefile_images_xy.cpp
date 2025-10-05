4727ac3990f18ee215013388c25a2e1c0168e74b
Dan Paulat <dpaulat@gmail.com>
2025-10-05 16:45:00 -0500

    Initial Placefile Images XY implementation

--- scwx-qt/source/scwx/qt/gl/draw/placefile_images_xy.cpp
+++ scwx-qt/source/scwx/qt/gl/draw/placefile_images_xy.cpp
@@ -1,4 +1,4 @@
-#include <scwx/qt/gl/draw/placefile_images.hpp>
+#include <scwx/qt/gl/draw/placefile_images_xy.hpp>
 #include <scwx/qt/types/placefile_types.hpp>
 #include <scwx/qt/util/maplibre.hpp>
 #include <scwx/qt/util/texture_atlas.hpp>
@@ -9,45 +9,34 @@
 #include <QUrl>
 #include <boost/unordered/unordered_flat_map.hpp>
 
-namespace scwx
-{
-namespace qt
-{
-namespace gl
-{
-namespace draw
+namespace scwx::qt::gl::draw
 {
 
-static const std::string logPrefix_ = "scwx::qt::gl::draw::placefile_images";
+static const std::string logPrefix_ = "scwx::qt::gl::draw::placefile_images_xy";
 static const auto        logger_    = scwx::util::Logger::Create(logPrefix_);
 
-static constexpr std::size_t kNumRectangles        = 1;
-static constexpr std::size_t kNumTriangles         = kNumRectangles * 2;
-static constexpr std::size_t kVerticesPerTriangle  = 3;
-static constexpr std::size_t kVerticesPerRectangle = kVerticesPerTriangle * 2;
-static constexpr std::size_t kPointsPerVertex      = 8;
-static constexpr std::size_t kPointsPerTexCoord    = 3;
+static constexpr std::size_t kNumRectangles       = 1;
+static constexpr std::size_t kNumTriangles        = kNumRectangles * 2;
+static constexpr std::size_t kVerticesPerTriangle = 3;
+static constexpr std::size_t kPointsPerVertex     = 8;
+static constexpr std::size_t kPointsPerTexCoord   = 3;
 static constexpr std::size_t kImageBufferLength =
    kNumTriangles * kVerticesPerTriangle * kPointsPerVertex;
 static constexpr std::size_t kTextureBufferLength =
    kNumTriangles * kVerticesPerTriangle * kPointsPerTexCoord;
 
-// Threshold, start time, end time
-static constexpr std::size_t kIntegersPerVertex_ = 3;
-
-class PlacefileImages::Impl
+class PlacefileImagesXY::Impl
 {
 public:
-   explicit Impl(const std::shared_ptr<GlContext>& context) :
-       context_ {context},
-       shaderProgram_ {nullptr},
-       vao_ {GL_INVALID_INDEX},
-       vbo_ {GL_INVALID_INDEX},
-       numVertices_ {0}
+   explicit Impl(const std::shared_ptr<GlContext>& context) : context_ {context}
    {
    }
+   ~Impl() = default;
 
-   ~Impl() {}
+   Impl(const Impl&)             = delete;
+   Impl& operator=(const Impl&)  = delete;
+   Impl(const Impl&&)            = delete;
+   Impl& operator=(const Impl&&) = delete;
 
    void UpdateBuffers();
    void UpdateTextureBuffer();
@@ -58,9 +47,6 @@ public:
    std::string baseUrl_ {};
 
    bool dirty_ {false};
-   bool thresholded_ {false};
-
-   std::chrono::system_clock::time_point selectedTime_ {};
 
    std::mutex imageMutex_;
 
@@ -69,68 +55,45 @@ public:
    boost::unordered_flat_map<std::string, types::PlacefileImageInfo>
       newImageFiles_ {};
 
-   std::vector<std::shared_ptr<const gr::Placefile::ImageDrawItem>>
+   std::vector<std::shared_ptr<const gr::Placefile::ImageXYDrawItem>>
       currentImageList_ {};
-   std::vector<std::shared_ptr<const gr::Placefile::ImageDrawItem>>
+   std::vector<std::shared_ptr<const gr::Placefile::ImageXYDrawItem>>
       newImageList_ {};
 
    std::vector<float> currentImageBuffer_ {};
-   std::vector<GLint> currentIntegerBuffer_ {};
    std::vector<float> newImageBuffer_ {};
-   std::vector<GLint> newIntegerBuffer_ {};
 
    std::vector<float> textureBuffer_ {};
 
-   std::shared_ptr<ShaderProgram> shaderProgram_;
+   std::shared_ptr<ShaderProgram> shaderProgram_ {nullptr};
 
    GLint uMVPMatrixLocation_ {static_cast<GLint>(GL_INVALID_INDEX)};
-   GLint uMapMatrixLocation_ {static_cast<GLint>(GL_INVALID_INDEX)};
-   GLint uOriginLatLongLocation_ {static_cast<GLint>(GL_INVALID_INDEX)};
-   GLint uMapDistanceLocation_ {static_cast<GLint>(GL_INVALID_INDEX)};
-   GLint uSelectedTimeLocation_ {static_cast<GLint>(GL_INVALID_INDEX)};
 
-   GLuint                vao_;
-   std::array<GLuint, 3> vbo_;
+   GLuint                vao_ {GL_INVALID_INDEX};
+   std::array<GLuint, 2> vbo_ {GL_INVALID_INDEX};
 
-   GLsizei numVertices_;
+   GLsizei numVertices_ {0};
 };
 
-PlacefileImages::PlacefileImages(const std::shared_ptr<GlContext>& context) :
+PlacefileImagesXY::PlacefileImagesXY(
+   const std::shared_ptr<GlContext>& context) :
     DrawItem(), p(std::make_unique<Impl>(context))
 {
 }
-PlacefileImages::~PlacefileImages() = default;
+PlacefileImagesXY::~PlacefileImagesXY() = default;
 
-PlacefileImages::PlacefileImages(PlacefileImages&&) noexcept = default;
-PlacefileImages&
-PlacefileImages::operator=(PlacefileImages&&) noexcept = default;
+PlacefileImagesXY::PlacefileImagesXY(PlacefileImagesXY&&) noexcept = default;
+PlacefileImagesXY&
+PlacefileImagesXY::operator=(PlacefileImagesXY&&) noexcept = default;
 
-void PlacefileImages::set_selected_time(
-   std::chrono::system_clock::time_point selectedTime)
-{
-   p->selectedTime_ = selectedTime;
-}
-
-void PlacefileImages::set_thresholded(bool thresholded)
-{
-   p->thresholded_ = thresholded;
-}
-
-void PlacefileImages::Initialize()
+void PlacefileImagesXY::Initialize()
 {
    p->shaderProgram_ = p->context_->GetShaderProgram(
-      {{GL_VERTEX_SHADER, ":/gl/geo_texture2d.vert"},
+      {{GL_VERTEX_SHADER, ":/gl/texture2d_array.vert"},
        {GL_GEOMETRY_SHADER, ":/gl/threshold.geom"},
        {GL_FRAGMENT_SHADER, ":/gl/texture2d_array.frag"}});
 
    p->uMVPMatrixLocation_ = p->shaderProgram_->GetUniformLocation("uMVPMatrix");
-   p->uMapMatrixLocation_ = p->shaderProgram_->GetUniformLocation("uMapMatrix");
-   p->uOriginLatLongLocation_ =
-      p->shaderProgram_->GetUniformLocation("uOriginLatLong");
-   p->uMapDistanceLocation_ =
-      p->shaderProgram_->GetUniformLocation("uMapDistance");
-   p->uSelectedTimeLocation_ =
-      p->shaderProgram_->GetUniformLocation("uSelectedTime");
 
    glGenVertexArrays(1, &p->vao_);
    glGenBuffers(static_cast<GLsizei>(p->vbo_.size()), p->vbo_.data());
@@ -143,7 +106,7 @@ void PlacefileImages::Initialize()
    // NOLINTBEGIN(performance-no-int-to-ptr)
    // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
 
-   // aLatLong
+   // aLatLong - TODO: aVertex
    glVertexAttribPointer(0,
                          2,
                          GL_FLOAT,
@@ -182,27 +145,8 @@ void PlacefileImages::Initialize()
                          static_cast<void*>(0));
    glEnableVertexAttribArray(2);
 
-   glBindBuffer(GL_ARRAY_BUFFER, p->vbo_[2]);
-   glBufferData(GL_ARRAY_BUFFER, 0u, nullptr, GL_DYNAMIC_DRAW);
-
-   // aThreshold
-   glVertexAttribIPointer(5, //
-                          1,
-                          GL_INT,
-                          kIntegersPerVertex_ * sizeof(GLint),
-                          static_cast<void*>(0));
-   glEnableVertexAttribArray(5);
-
-   // aTimeRange
-   glVertexAttribIPointer(6, //
-                          2,
-                          GL_INT,
-                          kIntegersPerVertex_ * sizeof(GLint),
-                          reinterpret_cast<void*>(1 * sizeof(GLint)));
-   glEnableVertexAttribArray(6);
-
    // aDisplayed
-   glVertexAttribI1i(7, 1);
+   glVertexAttribI1i(5, 1);
 
    // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
    // NOLINTEND(performance-no-int-to-ptr)
@@ -211,7 +155,7 @@ void PlacefileImages::Initialize()
    p->dirty_ = true;
 }
 
-void PlacefileImages::Render(
+void PlacefileImagesXY::Render(
    const QMapLibre::CustomLayerRenderParameters& params,
    bool                                          textureAtlasChanged)
 {
@@ -224,32 +168,6 @@ void PlacefileImages::Render(
       p->Update(textureAtlasChanged);
       p->shaderProgram_->Use();
       UseRotationProjection(params, p->uMVPMatrixLocation_);
-      UseMapProjection(
-         params, p->uMapMatrixLocation_, p->uOriginLatLongLocation_);
-
-      if (p->thresholded_)
-      {
-         // If thresholding is enabled, set the map distance
-         units::length::nautical_miles<float> mapDistance =
-            util::maplibre::GetMapDistance(params);
-         glUniform1f(p->uMapDistanceLocation_, mapDistance.value());
-      }
-      else
-      {
-         // If thresholding is disabled, set the map distance to 0
-         glUniform1f(p->uMapDistanceLocation_, 0.0f);
-      }
-
-      // Selected time
-      std::chrono::system_clock::time_point selectedTime =
-         (p->selectedTime_ == std::chrono::system_clock::time_point {}) ?
-            scwx::util::time::now() :
-            p->selectedTime_;
-      glUniform1i(
-         p->uSelectedTimeLocation_,
-         static_cast<GLint>(std::chrono::duration_cast<std::chrono::minutes>(
-                               selectedTime.time_since_epoch())
-                               .count()));
 
       // Interpolate texture coordinates
       glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -260,7 +178,7 @@ void PlacefileImages::Render(
    }
 }
 
-void PlacefileImages::Deinitialize()
+void PlacefileImagesXY::Deinitialize()
 {
    glDeleteVertexArrays(1, &p->vao_);
    glDeleteBuffers(static_cast<GLsizei>(p->vbo_.size()), p->vbo_.data());
@@ -270,11 +188,10 @@ void PlacefileImages::Deinitialize()
    p->currentImageList_.clear();
    p->currentImageFiles_.clear();
    p->currentImageBuffer_.clear();
-   p->currentIntegerBuffer_.clear();
    p->textureBuffer_.clear();
 }
 
-void PlacefileImages::StartImages(const std::string& baseUrl)
+void PlacefileImagesXY::StartImagesXY(const std::string& baseUrl)
 {
    p->baseUrl_ = baseUrl;
 
@@ -282,11 +199,10 @@ void PlacefileImages::StartImages(const std::string& baseUrl)
    p->newImageList_.clear();
    p->newImageFiles_.clear();
    p->newImageBuffer_.clear();
-   p->newIntegerBuffer_.clear();
 }
 
-void PlacefileImages::AddImage(
-   const std::shared_ptr<gr::Placefile::ImageDrawItem>& di)
+void PlacefileImagesXY::AddImageXY(
+   const std::shared_ptr<gr::Placefile::ImageXYDrawItem>& di)
 {
    if (di != nullptr)
    {
@@ -294,7 +210,7 @@ void PlacefileImages::AddImage(
    }
 }
 
-void PlacefileImages::FinishImages()
+void PlacefileImagesXY::FinishImagesXY()
 {
    // Update buffers
    p->UpdateBuffers();
@@ -305,25 +221,20 @@ void PlacefileImages::FinishImages()
    p->currentImageList_.swap(p->newImageList_);
    p->currentImageFiles_.swap(p->newImageFiles_);
    p->currentImageBuffer_.swap(p->newImageBuffer_);
-   p->currentIntegerBuffer_.swap(p->newIntegerBuffer_);
 
    // Clear the new buffers
    p->newImageList_.clear();
    p->newImageFiles_.clear();
    p->newImageBuffer_.clear();
-   p->newIntegerBuffer_.clear();
 
    // Mark the draw item dirty
    p->dirty_ = true;
 }
 
-void PlacefileImages::Impl::UpdateBuffers()
+void PlacefileImagesXY::Impl::UpdateBuffers()
 {
    newImageBuffer_.clear();
    newImageBuffer_.reserve(newImageList_.size() * kImageBufferLength);
-   newIntegerBuffer_.clear();
-   newIntegerBuffer_.reserve(newImageList_.size() * kVerticesPerRectangle *
-                             kIntegersPerVertex_);
    newImageFiles_.clear();
 
    // Fixed modulate color
@@ -331,6 +242,9 @@ void PlacefileImages::Impl::UpdateBuffers()
    static const float mc1 = 1.0f;
    static const float mc2 = 1.0f;
    static const float mc3 = 1.0f;
+
+   static constexpr float xOffset = 0.0f;
+   static constexpr float yOffset = 0.0f;
 
    for (auto& di : newImageList_)
    {
@@ -340,43 +254,23 @@ void PlacefileImages::Impl::UpdateBuffers()
                              std::forward_as_tuple(types::PlacefileImageInfo {
                                 di->imageFile_, baseUrl_}));
 
-      // Threshold value
-      units::length::nautical_miles<double> threshold = di->threshold_;
-      GLint thresholdValue = static_cast<GLint>(std::round(threshold.value()));
-
-      // Start and end time
-      GLint startTime =
-         static_cast<GLint>(std::chrono::duration_cast<std::chrono::minutes>(
-                               di->startTime_.time_since_epoch())
-                               .count());
-      GLint endTime =
-         static_cast<GLint>(std::chrono::duration_cast<std::chrono::minutes>(
-                               di->endTime_.time_since_epoch())
-                               .count());
-
       // Limit processing to groups of 3 (triangles)
       std::size_t numElements = di->elements_.size() - di->elements_.size() % 3;
       for (std::size_t i = 0; i < numElements; ++i)
       {
          auto& element = di->elements_[i];
 
-         // Latitude and longitude coordinates in degrees
-         const float lat = static_cast<float>(element.latitude_);
-         const float lon = static_cast<float>(element.longitude_);
-
-         // Base X/Y offsets in pixels
-         const float x = static_cast<float>(element.x_);
-         const float y = static_cast<float>(element.y_);
+         // X and Y coordinates in pixels
+         const auto x = static_cast<float>(element.x_);
+         const auto y = static_cast<float>(element.y_);
 
          newImageBuffer_.insert(newImageBuffer_.end(),
-                                {lat, lon, x, y, mc0, mc1, mc2, mc3});
-         newIntegerBuffer_.insert(newIntegerBuffer_.end(),
-                                  {thresholdValue, startTime, endTime});
+                                {x, y, xOffset, yOffset, mc0, mc1, mc2, mc3});
       }
    }
 }
 
-void PlacefileImages::Impl::UpdateTextureBuffer()
+void PlacefileImagesXY::Impl::UpdateTextureBuffer()
 {
    textureBuffer_.clear();
    textureBuffer_.reserve(currentImageList_.size() * kTextureBufferLength);
@@ -391,7 +285,7 @@ void PlacefileImages::Impl::UpdateTextureBuffer()
             currentImageFiles_.cbegin()->second :
             it->second;
 
-      const float r = static_cast<float>(image.texture_.layerId_);
+      const auto r = static_cast<float>(image.texture_.layerId_);
 
       // Limit processing to groups of 3 (triangles)
       std::size_t numElements = di->elements_.size() - di->elements_.size() % 3;
@@ -400,17 +294,17 @@ void PlacefileImages::Impl::UpdateTextureBuffer()
          auto& element = di->elements_[i];
 
          // Texture coordinates
-         const float s =
-            image.texture_.sLeft_ + (image.scaledWidth_ * element.tu_);
-         const float t =
-            image.texture_.tTop_ + (image.scaledHeight_ * element.tv_);
+         const auto s = static_cast<float>(image.texture_.sLeft_ +
+                                           (image.scaledWidth_ * element.tu_));
+         const auto t = static_cast<float>(image.texture_.tTop_ +
+                                           (image.scaledHeight_ * element.tv_));
 
          textureBuffer_.insert(textureBuffer_.end(), {s, t, r});
       }
    }
 }
 
-void PlacefileImages::Impl::Update(bool textureAtlasChanged)
+void PlacefileImagesXY::Impl::Update(bool textureAtlasChanged)
 {
    // If the texture atlas has changed
    if (dirty_ || textureAtlasChanged)
@@ -444,14 +338,6 @@ void PlacefileImages::Impl::Update(bool textureAtlasChanged)
          currentImageBuffer_.data(),
          GL_DYNAMIC_DRAW);
 
-      // Buffer threshold data
-      glBindBuffer(GL_ARRAY_BUFFER, vbo_[2]);
-      glBufferData(
-         GL_ARRAY_BUFFER,
-         static_cast<GLsizeiptr>(sizeof(GLint) * currentIntegerBuffer_.size()),
-         currentIntegerBuffer_.data(),
-         GL_DYNAMIC_DRAW);
-
       numVertices_ =
          static_cast<GLsizei>(currentImageBuffer_.size() / kPointsPerVertex);
    }
@@ -459,7 +345,4 @@ void PlacefileImages::Impl::Update(bool textureAtlasChanged)
    dirty_ = false;
 }
 
-} // namespace draw
-} // namespace gl
-} // namespace qt
-} // namespace scwx
+} // namespace scwx::qt::gl::draw
