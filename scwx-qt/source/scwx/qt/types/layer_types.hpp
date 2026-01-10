d3455d81d121c635d168cf25f136924947f951d1
TornadoRTX <143273950+TornadoRTX@users.noreply.github.com>
2026-01-10 10:48:37 -0500

    Refactor layer types and add LayerInfo structure

--- scwx-qt/source/scwx/qt/types/layer_types.hpp
+++ scwx-qt/source/scwx/qt/types/layer_types.hpp
@@ -1,33 +1,93 @@
 #pragma once
 
+#include <scwx/awips/phenomenon.hpp>
+#include <scwx/qt/types/map_types.hpp>
+#include <scwx/util/iterator.hpp>
+
+#include <array>
 #include <string>
+#include <variant>
+
+#include <boost/container/stable_vector.hpp>
+#include <boost/json/conversion.hpp>
+#include <boost/json/value.hpp>
 
 namespace scwx::qt::types
 {
 
-static constexpr std::size_t kMapCount_ = 9u;
-
-enum class AnimationState
+enum class LayerType
 {
-   Play,
-   Pause
+   Map,
+   Radar,
+   Alert,
+   Placefile,
+   Information,
+   Data,
+   Unknown
 };
 
-enum class MapTime
+enum class DataLayer
 {
-   Live,
-   Archive
+   OverlayProduct,
+   RadarRange,
+   Unknown
+};
+using DataLayerIterator = scwx::util::
+   Iterator<DataLayer, DataLayer::OverlayProduct, DataLayer::RadarRange>;
+
+enum class InformationLayer
+{
+   MapOverlay,
+   RadarSite,
+   ColorTable,
+   Markers,
+   Unknown
 };
 
-enum class NoUpdateReason
+enum class MapLayer
 {
-   NoChange,
-   NotLoaded,
-   NotAvailable,
-   InvalidProduct,
-   InvalidData
+   MapSymbology,
+   MapUnderlay,
+   Unknown
 };
 
-std::string GetMapTimeName(MapTime mapTime);
+using LayerDescription = std::variant<std::monostate,
+                                      DataLayer,
+                                      InformationLayer,
+                                      MapLayer,
+                                      awips::Phenomenon,
+                                      std::string>;
+
+struct LayerInfo
+{
+   LayerType                    type_ {LayerType::Unknown};
+   LayerDescription             description_;
+   bool                         movable_ {true};
+   std::array<bool, kMapCount_> displayed_ {true};
+};
+
+using LayerVector = boost::container::stable_vector<LayerInfo>;
+
+LayerType   GetLayerType(const std::string& name);
+std::string GetLayerTypeName(LayerType layerType);
+
+DataLayer   GetDataLayer(const std::string& name);
+std::string GetDataLayerName(DataLayer layer);
+
+InformationLayer GetInformationLayer(const std::string& name);
+std::string      GetInformationLayerName(InformationLayer layer);
+
+MapLayer    GetMapLayer(const std::string& name);
+std::string GetMapLayerName(MapLayer layer);
+
+std::string GetLayerDescriptionName(LayerDescription description);
+
+std::string GetLayerName(LayerType type, LayerDescription description);
+
+void      tag_invoke(boost::json::value_from_tag,
+                     boost::json::value& jv,
+                     const LayerInfo&    record);
+LayerInfo tag_invoke(boost::json::value_to_tag<LayerInfo>,
+                     const boost::json::value& jv);
 
 } // namespace scwx::qt::types
