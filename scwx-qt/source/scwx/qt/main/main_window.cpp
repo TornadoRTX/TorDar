3c7e692c0c1a9e83fa150b180f76385aaff40906
TornadoRTX <143273950+TornadoRTX@users.noreply.github.com>
2026-01-18 21:02:30 -0500

    Updated main_window.cpp: Rewrote so that it would be compatiable with clang-format v19.
    This is like the 4th time now?

--- scwx-qt/source/scwx/qt/main/main_window.cpp
+++ scwx-qt/source/scwx/qt/main/main_window.cpp
@@ -833,20 +833,18 @@ void MainWindowImpl::ConfigureMapLayout()
       connect(hs, &QSplitter::splitterMoved, this, MoveSplitter);
    }
 
-// Create overlay container.
-QWidget* map_overlay_container = new QWidget(mainWindow_->ui->centralwidget);
-QGridLayout* overlay_layout = new QGridLayout(map_overlay_container);
-overlay_layout->setContentsMargins(0, 0, 0, 0);
-overlay_layout->setSpacing(0);
-
-// Add map splitter (the maps themselves).
-overlay_layout->addWidget(vs, 0, 0);
-
-// Add overlay container to central widget.
-mainWindow_->ui->centralwidget->layout()->addWidget(map_overlay_container);
+   // Create overlay container
+   QWidget* mapOverlayContainer = new QWidget(mainWindow_->ui->centralwidget);
+   QGridLayout* overlayLayout   = new QGridLayout(mapOverlayContainer);
+   overlayLayout->setContentsMargins(0, 0, 0, 0);
+   overlayLayout->setSpacing(0);
+   // Add map splitter (the maps themselves)
+   overlayLayout->addWidget(vs, 0, 0);
+   // Add overlay container to central widget
+   mainWindow_->ui->centralwidget->layout()->addWidget(mapOverlayContainer);
 
    if (mapCount > 0)
-   {  
+   {
       SetActiveMap(maps_.at(0));
    }
    else
