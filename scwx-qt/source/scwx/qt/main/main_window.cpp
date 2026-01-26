fdd029225da08fd85fbe66e60848a5f6b7116f01
TornadoRTX <143273950+TornadoRTX@users.noreply.github.com>
2026-01-25 20:24:18 -0500

    Updated main_window.cpp: Created new Canary Toolbox

--- scwx-qt/source/scwx/qt/main/main_window.cpp
+++ scwx-qt/source/scwx/qt/main/main_window.cpp
@@ -256,12 +256,6 @@ MainWindow::MainWindow(QWidget* parent) :
     ui(new Ui::MainWindow)
 {
    ui->setupUi(this);
-   // Force Radar Toolbox to bottom-left
-   addDockWidget(Qt::LeftDockWidgetArea, ui->radarToolboxDock);
-   setCorner(Qt::BottomLeftCorner, Qt::LeftDockWidgetArea);
-
-   // Optional safety
-   ui->radarToolboxDock->setFloating(false);
 
    p->InitializeLayerDisplayActions();
 
@@ -364,6 +358,17 @@ MainWindow::MainWindow(QWidget* parent) :
       p->level3ProductsWidget_);
    ui->radarToolboxScrollAreaContents->layout()->addWidget(
       p->level3ProductsGroup_);
+
+   // === TEMP: Radar Toolbox Canary ===
+   auto* canaryGroup =
+      new ui::CollapsibleGroup(tr("Radar Toolbox Canary"), this);
+
+   auto* canaryLabel = new QLabel(
+      tr("If you can see this, the radar toolbox layout works."), this);
+
+   canaryGroup->GetContentsLayout()->addWidget(canaryLabel);
+
+   ui->radarToolboxScrollAreaContents->layout()->addWidget(canaryGroup);
 
    // Add Level 2 Settings
    p->level2SettingsGroup_ =
