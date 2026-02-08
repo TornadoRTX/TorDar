7904eef28c8f8cafc48c353da1be5d64bf01289b
Aryan Khan <143273950+TornadoRTX@users.noreply.github.com>
2026-02-08 09:59:39 -0500

    Updated scwx-qt.cmake: Added RadarProductCard.hpp to Directory list.

--- scwx-qt/scwx-qt.cmake
+++ scwx-qt/scwx-qt.cmake
@@ -297,7 +297,7 @@ set(HDR_UI source/scwx/qt/ui/about_dialog.hpp
            source/scwx/qt/ui/level2_settings_widget.hpp
            source/scwx/qt/ui/level3_products_widget.hpp
            source/scwx/qt/ui/radar_toolbox_widget/radar_toolbox_widget.hpp
-           source/scwx/qt/ui/radar_toolbox_widget/RadarProductCard.cpp
+           source/scwx/qt/ui/radar_toolbox_widget/RadarProductCard.hpp
            source/scwx/qt/ui/line_label.hpp
            source/scwx/qt/ui/open_url_dialog.hpp
            source/scwx/qt/ui/placefile_dialog.hpp
