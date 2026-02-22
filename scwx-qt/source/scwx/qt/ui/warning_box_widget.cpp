65777cba21b0480f70caefe7c1536a7f8ed6861b
Aryan Khan <143273950+TornadoRTX@users.noreply.github.com>
2026-02-22 15:21:02 -0500

    Updated warning_box_widget.cpp: Moved areas/source parameters to the right, added red Tornado Possible box for SVR T-STORM warnings with red glow, added yellow SVR T-STORM damage threat with yellow glow.

--- scwx-qt/source/scwx/qt/ui/warning_box_widget.cpp
+++ scwx-qt/source/scwx/qt/ui/warning_box_widget.cpp
@@ -22,6 +22,7 @@
 #include <QGraphicsDropShadowEffect>
 #include <QColor>
 #include <QFont>
+#include <QFontDatabase>
 #include <QFontMetrics>
 #include <QLayoutItem>
 #include <QMargins>
@@ -43,6 +44,22 @@ namespace ui
 
 static const std::string logPrefix_ = "scwx::qt::ui::warning_box_widget";
 static const auto        logger_    = util::Logger::Create(logPrefix_);
+
+static std::string LoadFontFamily(const QString& resourcePath,
+                                  const char*    fallbackFamily)
+{
+   const int fontId = QFontDatabase::addApplicationFont(resourcePath);
+   if (fontId >= 0)
+   {
+      const QStringList families =
+         QFontDatabase::applicationFontFamilies(fontId);
+      if (!families.isEmpty())
+      {
+         return families.first().toStdString();
+      }
+   }
+   return fallbackFamily;
+}
 
 class WarningBoxWidgetImpl : public QObject
 {
@@ -158,6 +175,11 @@ public:
    int                                        fixedWidth_ {0};
    QWidget*                                   detailsContainer_ {nullptr};
    QVBoxLayout*                               detailsLayout_ {nullptr};
+   std::string warningTitleFontFamily_ {"Rajdhani-Bold"};
+   std::string expirationFontFamily_ {"AlegreyaSans-ExtraBold"};
+   std::string monoFontFamily_ {"RobotoMono-Regular"};
+   std::string areaSourceLabelFontFamily_ {"AlegreyaSans-ExtraBold"};
+   std::string areaSourceValueFontFamily_ {"Rajdhani"};
 };
 
 WarningBoxWidget::WarningBoxWidget(QWidget* parent) :
@@ -167,6 +189,17 @@ WarningBoxWidget::WarningBoxWidget(QWidget* parent) :
 {
    ui->setupUi(this);
 
+   p->warningTitleFontFamily_ =
+      LoadFontFamily(":/res/fonts/Rajdhani-Bold.ttf", "Rajdhani");
+   p->expirationFontFamily_ = LoadFontFamily(
+      ":/res/fonts/AlegreyaSans-ExtraBold.ttf", "AlegreyaSans-ExtraBold");
+   p->monoFontFamily_ = LoadFontFamily(":/res/fonts/RobotoMono-Regular.ttf",
+                                       "RobotoMono-Regular");
+   p->areaSourceLabelFontFamily_ = LoadFontFamily(
+      ":/res/fonts/AlegreyaSans-ExtraBold.ttf", "AlegreyaSans-ExtraBold");
+   p->areaSourceValueFontFamily_ =
+      LoadFontFamily(":/res/fonts/Rajdhani-Bold.ttf", "Rajdhani");
+
    setWindowFlags(Qt::Widget | Qt::FramelessWindowHint);
    setAttribute(Qt::WA_TranslucentBackground, false);
    setAttribute(Qt::WA_StyledBackground, true);
@@ -175,9 +208,13 @@ WarningBoxWidget::WarningBoxWidget(QWidget* parent) :
    ui->warningTypeLabel->setTextInteractionFlags(Qt::NoTextInteraction);
    ui->warningTypeLabel->setWordWrap(true);
    ui->warningTypeLabel->setStyleSheet(
-      "font-size: 20px; font-weight: 800; letter-spacing: 1px;");
+      QString("font-family: '%1';"
+              "font-size: 20px; font-weight: 700; letter-spacing: 1px;")
+         .arg(QString::fromStdString(p->warningTitleFontFamily_)));
    ui->expirationLabel->setStyleSheet(
-      "font-size: 13px; font-weight: 700; letter-spacing: 0.5px;");
+      QString("font-family: '%1';"
+              "font-size: 13px; font-weight: 700; letter-spacing: 0.5px;")
+         .arg(QString::fromStdString(p->expirationFontFamily_)));
    ui->viewEasTextButton->setText("VIEW FULL EAS TEXT");
    ui->closeButton->setText("x");
    ui->closeButton->setFixedSize(28, 28);
@@ -413,6 +450,16 @@ void WarningBoxWidgetImpl::AddDetailRow(const std::string& label,
    valueW->setObjectName("detailValue");
    valueW->setWordWrap(true);
    valueW->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+
+   if (label == "Areas" || label == "Source")
+   {
+      labelW->setStyleSheet(
+         QString("font-family: '%1'; font-weight: 700; letter-spacing: 0px;")
+            .arg(QString::fromStdString(areaSourceLabelFontFamily_)));
+      valueW->setStyleSheet(
+         QString("font-family: '%1'; font-weight: 700; letter-spacing: 0px;")
+            .arg(QString::fromStdString(areaSourceValueFontFamily_)));
+   }
 
    QHBoxLayout* row = new QHBoxLayout(rowFrame);
    row->setContentsMargins(8, 6, 8, 6);
@@ -659,6 +706,7 @@ void WarningBoxWidgetImpl::ApplyTheme(
    styleSheet += "  letter-spacing: 0.5px;";
    styleSheet += "}";
    styleSheet += "QWidget#WarningBoxWidget QLabel#statesLabel {";
+   styleSheet += "  font-family: '" + monoFontFamily_ + "';";
    styleSheet += "  font-size: 16px;";
    styleSheet += "  font-weight: 700;";
    styleSheet += "  letter-spacing: 0.8px;";
@@ -669,6 +717,7 @@ void WarningBoxWidgetImpl::ApplyTheme(
    styleSheet += "  background-color: rgba(19, 56, 110, 220);";
    styleSheet += "}";
    styleSheet += "QWidget#WarningBoxWidget QLabel#stateBadgeLabel {";
+   styleSheet += "  font-family: '" + monoFontFamily_ + "';";
    styleSheet += "  color: rgb(224, 236, 255);";
    styleSheet += "  font-size: 11px;";
    styleSheet += "  font-weight: 700;";
@@ -689,6 +738,7 @@ void WarningBoxWidgetImpl::ApplyTheme(
    styleSheet += "  background-color: rgba(13, 15, 29, 236);";
    styleSheet += "}";
    styleSheet += "QWidget#WarningBoxWidget QLabel#severeMetricTitle {";
+   styleSheet += "  font-family: '" + monoFontFamily_ + "';";
    styleSheet += "  font-size: 10px;";
    styleSheet += "  font-weight: 700;";
    styleSheet += "  letter-spacing: 0.7px;";
@@ -781,6 +831,7 @@ void WarningBoxWidgetImpl::ApplyTheme(
       styleSheet += "  background-color: rgb(42, 31, 27);";
       styleSheet += "}";
       styleSheet += "QWidget#WarningBoxWidget QLabel#severeMetricTitle {";
+      styleSheet += "  font-family: '" + monoFontFamily_ + "';";
       styleSheet += "  color: rgba(202, 186, 147, 235);";
       styleSheet += "  letter-spacing: 0.9px;";
       styleSheet += "}";
@@ -857,7 +908,8 @@ void WarningBoxWidgetImpl::UpdateTitleFont()
 {
    const int targetWidth = std::max(120, self_->ui->warningTypeLabel->width());
    QFont     font        = self_->ui->warningTypeLabel->font();
-   int       pointSize   = 20;
+   font.setFamily(QString::fromStdString(warningTitleFontFamily_));
+   int pointSize = 20;
    font.setPointSize(pointSize);
 
    while (pointSize > 12)
