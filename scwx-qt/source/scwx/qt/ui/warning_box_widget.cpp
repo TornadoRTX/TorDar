4f885112428380c800e026e51841ab7447e5c95f
Aryan Khan <143273950+TornadoRTX@users.noreply.github.com>
2026-03-07 13:56:14 -0500

    Changed top right corner opacity to 2%.

--- scwx-qt/source/scwx/qt/ui/warning_box_widget.cpp
+++ scwx-qt/source/scwx/qt/ui/warning_box_widget.cpp
@@ -961,10 +961,10 @@ void WarningBoxWidgetImpl::ApplyTheme(
       "cx:1.0, cy:0.0, radius:1.35, fx:1.0, fy:0.0, "
       "stop:0 rgba(" +
       cornerGlowColor +
-      ", 250), "
+      ", 5), "
       "stop:0.38 rgba(" +
       cornerGlowColor +
-      ", 155), "
+      ", 3), "
       "stop:0.60 rgba(0, 2, 26, 255), "
       "stop:1 rgba(0, 2, 26, 255));";
    styleSheet += "  border: 1px solid rgba(" + accentColor + ", 220);";
