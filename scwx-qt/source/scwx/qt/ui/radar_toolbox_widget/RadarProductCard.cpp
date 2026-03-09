4a748e3dde8f2474b37ef12ed8e43ecd9e5e6752
Aryan Khan <143273950+TornadoRTX@users.noreply.github.com>
2026-03-08 20:17:48 -0400

    Product Labels should now use 'Inter Tight Regular'

--- scwx-qt/source/scwx/qt/ui/radar_toolbox_widget/RadarProductCard.cpp
+++ scwx-qt/source/scwx/qt/ui/radar_toolbox_widget/RadarProductCard.cpp
@@ -14,15 +14,15 @@ namespace scwx::qt::ui
 
 namespace
 {
-QString GetOswaldMediumFontFamily()
+QString GetInterTightRegularFontFamily()
 {
    static QString fontFamily {};
    static bool    loaded {false};
 
    if (!loaded)
    {
-      const int fontId =
-         QFontDatabase::addApplicationFont(":/res/fonts/Oswald-Medium.ttf");
+      const int fontId = QFontDatabase::addApplicationFont(
+         ":/res/fonts/InterTight-Regular.ttf");
 
       if (fontId != -1)
       {
@@ -98,9 +98,9 @@ RadarProductCard::RadarProductCard(const QString& title,
    titleLabel_->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
    titleLabel_->setMinimumWidth(0);
    titleLabel_->setProperty("selected", false);
-   QFont titleFont {GetOswaldMediumFontFamily()};
+   QFont titleFont {GetInterTightRegularFontFamily()};
    titleFont.setPixelSize(11);
-   titleFont.setWeight(QFont::Medium);
+   titleFont.setWeight(QFont::Normal);
    titleLabel_->setFont(titleFont);
 
    // -------------------------
